3f06a8125e68990bc6cf0577ff6a82057d6c13e3
Nadine-15 <200395742@student.georgianc.on.ca>
2021-02-24 14:32:37 -0400

    Contact page

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.get('/about', function(req, res, next) {
 });
 
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact Me' });
+  res.render('contact', { title: 'Contact Me' });
 });
 
 router.get('/projects', function(req, res, next) {
