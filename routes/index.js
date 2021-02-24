b388e152e0b325270e4bfee94500b6da99961674
Nadine-15 <200395742@student.georgianc.on.ca>
2021-02-24 13:07:34 -0400

    Home Page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'HomePage' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About Me' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact Me' });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
 });
 
 module.exports = router;
